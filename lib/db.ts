be12fbedac027e7d465643647e3ed48981760d09
kajialsoad <123802651+kajialsoad@users.noreply.github.com>
2025-07-12 14:28:44 +0600

    up

--- lib/db.ts
+++ lib/db.ts
@@ -2,6 +2,13 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const db = globalForPrisma.prisma || new PrismaClient();
+export const db = globalForPrisma.prisma || new PrismaClient({
+  log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
