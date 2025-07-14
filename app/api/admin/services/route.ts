a4b3614e45d9927562715db002919dd39674d41a
kajialsoad <123802651+kajialsoad@users.noreply.github.com>
2025-07-14 17:46:45 +0600

    mq

--- app/api/admin/services/route.ts
+++ app/api/admin/services/route.ts
@@ -38,6 +38,7 @@ export async function GET(request: Request) {
         },
         include: {
           category: true,
+          serviceType: true,
         },
       }),
       db.service.count({ where: whereClause }),
