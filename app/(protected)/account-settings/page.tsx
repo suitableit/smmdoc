7a8310466eef303b06d554ebd968f97a89e277e2
kajialsoad <123802651+kajialsoad@users.noreply.github.com>
2025-07-17 00:40:52 +0600

    new

--- app/(protected)/account-settings/page.tsx
+++ app/(protected)/account-settings/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-/* eslint-disable @typescript-eslint/no-unused-vars  */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { useCurrentUser } from '@/hooks/use-current-user';
