7d77644cc5a275bba3f056551f1404ca3dbd85d9
suitableit <admin@suitableit.com>
2025-11-12 20:50:22 +0600

    Last

--- app/(protected)/admin/settings/currency/page.tsx
+++ app/(protected)/admin/settings/currency/page.tsx
@@ -672,7 +672,6 @@ const PaymentCurrencyPage = () => {
                   >
                     {isUpdatingRates ? (
                       <>
-                        <div className="animate-spin rounded-full h-4 w-4 border-b-2 border-white"></div>
                         Fixing...
                       </>
                     ) : (
