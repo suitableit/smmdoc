bbe0a5f45e576f3b53e407308ebfe433a20042a1
suitableit <admin@suitableit.com>
2025-11-15 09:47:51 +0600

    Last

--- prisma/migrations/20250125000002_remove_redundant_api_columns/migration.sql
+++
@@ -1,7 +0,0 @@
--- Remove redundant API columns that duplicate existing functionality
--- apiServiceId is replaced by providerServiceId
--- apiProviderId is replaced by providerId
-
--- Remove redundant columns
-ALTER TABLE `services` DROP COLUMN IF EXISTS `apiServiceId`;
-ALTER TABLE `services` DROP COLUMN IF EXISTS `apiProviderId`;
--- prisma/migrations/20250125000006_add_order_link_to_services/migration.sql
+++
@@ -1,4 +0,0 @@
--- Add orderLink column to services table if it doesn't exist
--- This column stores the order link type: "link" or "username"
-
-ALTER TABLE `services` ADD COLUMN IF NOT EXISTS `orderLink` VARCHAR(191) NOT NULL DEFAULT 'link';
